c4eb0f6d77b85c4026835882ba71527f723a52df
rafikhan06-Cap <rafikhan3488@gmail.com>
2025-10-12 22:37:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,21 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+  const prices = document.querySelectorAll(".price");
+	let total = 0;
+	prices.forEach(priceCell => {
+		total +=Number(priceCell.textContent)
+	});
+
+	const newRow = document.createElement("tr");
+	const newCell = document.createElement("td");
+
+	newCell.setAttribute("colspan", "2");
+	newCell.textContent = `Total Price: Rs ${total}`;
+	newCell.style.textAlign="center";
+
+	newRow.appendChild(newCell);
+	document.querySelector("table").appendChild(newRow);
 };
 
 getSumBtn.addEventListener("click", getSum);
